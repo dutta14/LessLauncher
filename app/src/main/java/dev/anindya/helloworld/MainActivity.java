403956b80a5681cdc6039f0572d2c2f7cd7baf2f
Anindya Dutta <anindya.dutta22@gmail.com>
2020-04-04 16:43:22 -0700

    Fix #7: Exclude from recents and override back button for home screen

--- app/src/main/java/dev/anindya/helloworld/MainActivity.java
+++ app/src/main/java/dev/anindya/helloworld/MainActivity.java
@@ -3,10 +3,10 @@ package dev.anindya.helloworld;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.GestureDetector;
 import android.view.MotionEvent;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.view.GestureDetectorCompat;
 
@@ -17,34 +17,38 @@ public class MainActivity extends AppCompatActivity {
     private GestureDetectorCompat mDetector;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(@NonNull final Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         mDetector = new GestureDetectorCompat(this, new MyGestureListener(this));
     }
 
     @Override
-    public boolean onTouchEvent(MotionEvent event) {
-        this.mDetector.onTouchEvent(event);
+    public boolean onTouchEvent(@NonNull final MotionEvent event) {
+        mDetector.onTouchEvent(event);
         return super.onTouchEvent(event);
     }
 
     class MyGestureListener extends GestureDetector.SimpleOnGestureListener {
-        private static final String DEBUG_TAG = "Gestures";
-
         private Context mContext;
 
-        private MyGestureListener(Context context) {
+        private MyGestureListener(@NonNull final Context context) {
             mContext = context;
         }
 
         @Override
-        public boolean onFling(MotionEvent event1, MotionEvent event2,
-                               float velocityX, float velocityY) {
-            Intent intent = new Intent();
+        public boolean onFling(@NonNull final MotionEvent event1,
+                               @NonNull final MotionEvent event2,
+                               final float velocityX, final float velocityY) {
+            final Intent intent = new Intent();
             intent.setClass(mContext, DrawerActivity.class);
             startActivity(intent);
             return true;
         }
+    }
+
+    @Override
+    public void onBackPressed() {
+        // do nothing.
     }
 }
