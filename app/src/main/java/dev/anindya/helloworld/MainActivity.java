69c563e8b07c79028024826800d64330673188d0
Anindya Dutta <anindya.dutta22@gmail.com>
2020-04-04 00:38:21 -0700

    Fix #3: Add an app drawer

--- app/src/main/java/dev/anindya/helloworld/MainActivity.java
+++ app/src/main/java/dev/anindya/helloworld/MainActivity.java
@@ -1,14 +1,50 @@
 package dev.anindya.helloworld;
 
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.GestureDetector;
+import android.view.MotionEvent;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.view.GestureDetectorCompat;
+
+import dev.anindya.helloworld.appdrawer.DrawerActivity;
 
 public class MainActivity extends AppCompatActivity {
+
+    private GestureDetectorCompat mDetector;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mDetector = new GestureDetectorCompat(this, new MyGestureListener(this));
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        this.mDetector.onTouchEvent(event);
+        return super.onTouchEvent(event);
+    }
+
+    class MyGestureListener extends GestureDetector.SimpleOnGestureListener {
+        private static final String DEBUG_TAG = "Gestures";
+
+        private Context mContext;
+
+        private MyGestureListener(Context context) {
+            mContext = context;
+        }
+
+        @Override
+        public boolean onFling(MotionEvent event1, MotionEvent event2,
+                               float velocityX, float velocityY) {
+            Intent intent = new Intent();
+            intent.setClass(mContext, DrawerActivity.class);
+            startActivity(intent);
+            return true;
+        }
     }
 }
