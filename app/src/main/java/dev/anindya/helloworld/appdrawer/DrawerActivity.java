ba836ae44c29bd4534f456d65305b53d80527b9d
Anindya Dutta <anindya.dutta22@gmail.com>
2020-04-04 22:16:01 -0700

    Add annotations to method calls

--- app/src/main/java/dev/anindya/helloworld/appdrawer/DrawerActivity.java
+++ app/src/main/java/dev/anindya/helloworld/appdrawer/DrawerActivity.java
@@ -2,6 +2,7 @@ package dev.anindya.helloworld.appdrawer;
 
 import android.os.Bundle;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
@@ -10,14 +11,16 @@ import dev.anindya.helloworld.R;
 
 public class DrawerActivity extends AppCompatActivity {
 
+    private static final int SPAN_COUNT = 4;
+
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(@Nullable final Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_drawer);
 
         final RecyclerView recyclerView = findViewById(R.id.icon_list);
         final RecyclerView.Adapter viewAdapter = new IconViewAdapter(this);
         recyclerView.setAdapter(viewAdapter);
-        recyclerView.setLayoutManager(new GridLayoutManager(this, 4));
+        recyclerView.setLayoutManager(new GridLayoutManager(this, SPAN_COUNT));
     }
 }
